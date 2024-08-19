f4282c1e0dcded33d62bb1a536056e84873a176d
timdecillis <timdecillis@gmail.com>
2024-08-19 15:18:12 -0700

    created Projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Projects() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <h1>HOME PAGE</h1>
-      <Link href="/projects">Projects</Link>
+      <h1>PROJECTS PAGE</h1>
+      <Link href="/">Home</Link>
       <Link href="/contact">Contact</Link>
     </main>
   );
