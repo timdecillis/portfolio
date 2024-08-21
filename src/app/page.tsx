e1121590e2fc08cd440430bb574e5a2485427aa0
timdecillis <timdecillis@gmail.com>
2024-08-20 19:55:23 -0700

    acorrected Image path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <h1>HOME PAGE</h1>
+      <Image width={500} height={500} src={"/Sibley (1).jpg"} />
       <Link href="/projects">Projects</Link>
       <Link href="/contact">Contact</Link>
     </main>
